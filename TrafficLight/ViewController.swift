dd417202eda76beef47588ef9b05ca42d6d409c7
user <user@example.com>
2023-05-02 13:38:40 +0300

    release TrafficLight 0.1

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    enum TrafficLightState {
+        case off
+        case red
+        case yellow
+        case green
+        
+        func isOff() -> Bool {
+            self == .off
+        }
     }
 
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var nextButton: UIButton!
 
+    var currentState = TrafficLightState.off
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshState(for: currentState)
+    }
+
+    @IBAction func nextButtonDidTapped(_ sender: Any) {
+        let statesWorkflow: [TrafficLightState: TrafficLightState] = [
+            .off: .red,
+            .red: .yellow,
+            .yellow: .green,
+            .green: .red,
+        ]
+        
+        currentState = statesWorkflow[currentState] ?? .off
+        
+        refreshState(for: currentState)
+    }
+
+    private func refreshState(for state: TrafficLightState) {
+        let statesViewMap: [TrafficLightState: (UIView, UIColor)] = [
+            .red:    (redLightView, .red),
+            .yellow: (yellowLightView, .yellow),
+            .green:  (greenLightView, .green),
+        ]
+        
+        for (lightState, (lightView, lightColor)) in statesViewMap {
+            lightView.layer.cornerRadius = lightView.frame.width / 2
+            lightView.backgroundColor = lightColor
+            lightView.alpha = state == lightState ? 1 : 0.3
+        }
+        
+        nextButton.setTitle(state.isOff() ? "START" : "NEXT", for: .normal)
+    }
+    
 }
 
